f5d4d52a6718d232caba16af59f7bbea54a39466
Daniel Cabral Correa <daniel.liboos@gmail.com>
2019-04-29 15:13:09 -0300

    Iniciando Mapa

---
+++ SR/Mapa.py
@@ -0,0 +1,23 @@
+from itertools import chain
+import codecs
+import collections
+import copy
+import random
+import struct
+import sys
+import socket
+from robo import *
+from SensorLum import *
+from enum import Enum
+
+class mapa:
+    def __init__(self):
+        self.numCacas = 0
+        self.numRobo = 0
+        self.tamMapa 
+        self.mapa
+
+
+
+
+
